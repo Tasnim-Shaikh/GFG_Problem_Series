5557af2fcce0211f5e88adb616be8eb52844ba05
Tasnim Shaikh <tasnimshaikh2003@gmail.com>
2025-02-24 19:13:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Stock span problem/stock-span-problem.java
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+
+        while (t-- > 0) {
+
+            ArrayList<Integer> array1 = new ArrayList<Integer>();
+            String line = read.readLine();
+            String[] tokens = line.split(" ");
+            for (String token : tokens) {
+                array1.add(Integer.parseInt(token));
+            }
+            ArrayList<Integer> v = new ArrayList<Integer>();
+            int[] arr = new int[array1.size()];
+            int idx = 0;
+            for (int i : array1) arr[idx++] = i;
+
+            v = new Solution().calculateSpan(arr);
+
+            for (int i = 0; i < v.size(); i++) System.out.print(v.get(i) + " ");
+
+            System.out.println();
+
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+
+class Solution {
+    public ArrayList<Integer> calculateSpan(int[] arr) {
+        // write code here
+            ArrayList<Integer> span = new ArrayList<>();
+        Stack<Integer> stack = new Stack<>();
+        
+        for (int i = 0; i < arr.length; i++) {
+            // Pop elements from the stack while the stack is not empty 
+            // and the current price is greater than or equal to the price at stack top
+            while (!stack.isEmpty() && arr[stack.peek()] <= arr[i]) {
+                stack.pop();
+            }
+            
+            // If stack is empty, span is (i + 1), otherwise span is (i - stack.peek())
+            int spanValue = stack.isEmpty() ? (i + 1) : (i - stack.peek());
+            span.add(spanValue);
+            
+            // Push current index to stack
+            stack.push(i);
+        }
+        
+        return span;
+    }
+}
