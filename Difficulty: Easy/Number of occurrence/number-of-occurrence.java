7e56e77b3aacc0baf0b3dcf6bec29f100c4be8c3
Tasnim Shaikh <tasnimshaikh2003@gmail.com>
2025-03-07 21:25:20 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Number of occurrence/number-of-occurrence.java
+++ Difficulty: Easy/Number of occurrence/number-of-occurrence.java
@@ -32,12 +32,64 @@ class GFG {
 class Solution {
     int countFreq(int[] arr, int target) {
         // code here
-        int count=0;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]==target){
-                count++;
+        //Approach 1:
+        // int count=0;
+        // for(int i=0;i<arr.length;i++){
+        //     if(arr[i]==target){
+        //         count++;
+        //     }
+        // }
+        // return count;
+        
+        
+        //Approach 2:
+        int c=findceil(arr,target);
+        int f=0;
+        if(c!=-1){
+            f=findfloor(arr,target);
+        }
+           else{
+               return 0;
+           }
+        
+        return (c-f)+1;
+    }
+    int findceil(int arr[],int target){
+        int left=0;
+        int right=arr.length-1;
+        int ans=-1;
+        while(left<=right){
+            int mid=(left+right)/2;
+            if(arr[mid]==target){
+                ans=mid;
+                left=mid+1;
+            }
+            else if(target>arr[mid]){
+                left=mid+1;
+            }
+            else{
+                right=mid-1;
             }
         }
-        return count;
+        return ans;
+    }
+    int findfloor(int arr[],int target){
+        int left=0;
+        int right=arr.length-1;
+        int ans=-1;
+        while(left<=right){
+            int mid=(left+right)/2;
+            if(arr[mid]==target){
+                ans=mid;
+                right=mid-1;
+            }
+            else if(target>arr[mid]){
+                left=mid+1;
+            }
+            else{
+                right=mid-1;
+            }
+        }
+        return ans;
     }
 }
